492cd8e803a881683cffcf22614ea3d74bf477c9
intermediosdigitales2020 <71674139+intermediosdigitales2020@users.noreply.github.com>
2022-09-29 15:50:25 -0500

    Update PORT 8081

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:8089"
 };
 
 app.use(cors(corsOptions));
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
 require("./routes/tutorial.routes.js")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
